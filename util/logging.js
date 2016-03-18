b2597d45de7cc178011d6366d66434ae6a3258c9
allenmichael <allenmichaelgrobelny@gmail.com>
2016-03-18 16:47:16 -0500

    Removed logging.js

--- util/logging.js
+++
@@ -1,9 +0,0 @@
-var util = require('util');
-
-module.exports.LogToConsole = function (result) {
-  console.log(util.inspect(result));
-}
-
-module.exports.ReportErrorToConsole = function (error) {
-  console.error("Reporting error ---- " + error.message, error);
-}
